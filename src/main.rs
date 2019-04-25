a47fe463d00149eeba8c96c9555955662c71189f
tarberd <bernardo.mferrari@gmail.com>
2019-04-25 19:34:46 -0300

    Create convertion from automata to regular grammar

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::collections::HashSet;
 
+#[derive(Debug, Clone)]
 struct Automata {
     start_state: String,
     transition_function: HashMap<(String, String), String>,
@@ -14,6 +15,46 @@ impl Automata {
             actual_state = self.transition_function[&(actual_state, symbol.to_string())].clone();
         }
         self.accept_states.contains(&actual_state)
+    }
+}
+
+#[derive(Debug, Clone)]
+struct RegularGrammar {
+    start_symble: String,
+    productions: HashMap<String, Vec<String>>,
+}
+
+fn state_to_symble(state: &String) -> String {
+    String::from("<") + state + ">"
+}
+
+fn make_regular_grammar(automata: &Automata) -> RegularGrammar {
+    let mut productions = HashMap::new();
+
+    for ((state, _), _) in &automata.transition_function {
+        productions.insert(state_to_symble(state), vec![]);
+    }
+
+    for ((state, entry_symble), next_state) in &automata.transition_function {
+        match productions.get_mut(&state_to_symble(state)) {
+            Some(x) => x.push(entry_symble.clone() + &state_to_symble(next_state)),
+            None => (),
+        }
+
+        if automata
+            .accept_states
+            .contains(&state_to_symble(next_state))
+        {
+            match productions.get_mut(&state_to_symble(state)) {
+                Some(x) => x.push(entry_symble.clone()),
+                None => (),
+            }
+        }
+    }
+
+    RegularGrammar {
+        start_symble: state_to_symble(&automata.start_state),
+        productions: productions,
     }
 }
 
@@ -35,4 +76,10 @@ fn main() {
     let result = automata.compute(input.as_str());
 
     println!("Result {}", result);
+
+    println!("{:?}", automata);
+
+    let regular_grammar = make_regular_grammar(&automata);
+
+    println!("{:?}", regular_grammar);
 }
