18a242b12d1b32d30c625f67b31adbc9c0c63c14
tarberd <bernardo.mferrari@gmail.com>
2019-04-25 14:30:37 -0300

    Fisrt automata draft

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
 use std::collections::HashMap;
+use std::collections::HashSet;
+
+struct Automata {
+    start_state: String,
+    transition_function: HashMap<(String, String), String>,
+    accept_states: HashSet<String>,
+}
+
+impl Automata {
+    fn compute(&self, input: &str) -> bool {
+        let mut actual_state = self.start_state.clone();
+        for symbol in input.chars() {
+            actual_state = self.transition_function[&(actual_state, symbol.to_string())].clone();
+        }
+        self.accept_states.contains(&actual_state)
+    }
+}
 
 fn main() {
-    let accept_state = "q0";
-    let input = "ababab";
+    let input = String::from("abababa");
     let mut hash = HashMap::new();
 
-    hash.insert(("q0", "b"), "q1");
-    hash.insert(("q0", "a"), "q0");
-    hash.insert(("q1", "a"), "q0");
-    hash.insert(("q1", "b"), "q1");
+    hash.insert((String::from("q0"), String::from("a")), String::from("q0"));
+    hash.insert((String::from("q0"), String::from("b")), String::from("q1"));
+    hash.insert((String::from("q1"), String::from("a")), String::from("q0"));
+    hash.insert((String::from("q1"), String::from("b")), String::from("q1"));
 
-    let mut actual_state = "q0";
+    let automata = Automata {
+        start_state: String::from("q0"),
+        transition_function: hash,
+        accept_states: [String::from("q0")].iter().cloned().collect(),
+    };
 
-    for symbol in input.chars() {
-        actual_state = hash[&(actual_state, symbol.to_string().as_str())];
-    }
+    let result = automata.compute(input.as_str());
 
-    println!("Entry: {} Finish State: {}", input, actual_state);
+    println!("Result {}", result);
 }
