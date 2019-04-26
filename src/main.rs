32100c3bcbda1b6352306db49c49872e1a1e7165
tarberd <bernardo.mferrari@gmail.com>
2019-04-25 21:11:00 -0300

    Create separated lib file

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,6 @@
+use formally_cool::make_regular_grammar;
+use formally_cool::Automata;
 use std::collections::HashMap;
-use std::collections::HashSet;
-
-#[derive(Debug, Clone)]
-struct Automata {
-    start_state: String,
-    transition_function: HashMap<(String, String), String>,
-    accept_states: HashSet<String>,
-}
-
-impl Automata {
-    fn compute(&self, input: &str) -> bool {
-        let mut actual_state = self.start_state.clone();
-        for symbol in input.chars() {
-            actual_state = self.transition_function[&(actual_state, symbol.to_string())].clone();
-        }
-        self.accept_states.contains(&actual_state)
-    }
-}
-
-#[derive(Debug, Clone)]
-struct RegularGrammar {
-    start_symble: String,
-    productions: HashMap<String, Vec<String>>,
-}
-
-fn state_to_symble(state: &String) -> String {
-    String::from("<") + state + ">"
-}
-
-fn make_regular_grammar(automata: &Automata) -> RegularGrammar {
-    let mut productions = HashMap::new();
-
-    for ((state, _), _) in &automata.transition_function {
-        productions.insert(state_to_symble(state), vec![]);
-    }
-
-    for ((state, entry_symble), next_state) in &automata.transition_function {
-        match productions.get_mut(&state_to_symble(state)) {
-            Some(x) => x.push(entry_symble.clone() + &state_to_symble(next_state)),
-            None => (),
-        }
-
-        if automata
-            .accept_states
-            .contains(&state_to_symble(next_state))
-        {
-            match productions.get_mut(&state_to_symble(state)) {
-                Some(x) => x.push(entry_symble.clone()),
-                None => (),
-            }
-        }
-    }
-
-    RegularGrammar {
-        start_symble: state_to_symble(&automata.start_state),
-        productions: productions,
-    }
-}
 
 fn main() {
     let input = String::from("abababa");
