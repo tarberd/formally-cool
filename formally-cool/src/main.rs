7c7e19128838a3f93792fd421c76fc456c09ff56
tarberd <bernardo.mferrari@gmail.com>
2019-05-06 23:51:55 -0300

    Finish implementation of nfa_to_dfa

--- formally-cool/src/main.rs
+++ formally-cool/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 use std::fs::File;
 use std::io::{BufReader, BufWriter};
@@ -7,7 +7,7 @@ use formally_cool::regular_languages::*;
 
 fn main() {
     let input = String::from("abababa");
-    let mut hash = HashMap::new();
+    let mut hash = BTreeMap::new();
 
     hash.insert((String::from("q0"), String::from("a")), String::from("q0"));
     hash.insert((String::from("q0"), String::from("b")), String::from("q1"));
@@ -15,8 +15,13 @@ fn main() {
     hash.insert((String::from("q1"), String::from("b")), String::from("q1"));
 
     let automata = DeterministicFiniteAutomata {
-        start_state: String::from("q0"),
+        states: ["q0".to_string(), "q1".to_string()]
+            .iter()
+            .cloned()
+            .collect(),
+        alphabet: ["a".to_string(), "b".to_string()].iter().cloned().collect(),
         transition_function: hash,
+        start_state: String::from("q0"),
         accept_states: [String::from("q0")].iter().cloned().collect(),
     };
 
@@ -33,32 +38,6 @@ fn main() {
     let automata = NondeterministicFiniteAutomata::from(&regular_grammar);
 
     println!("{:#?}", automata);
-
-    let serialized = serde_yaml::to_string(&automata).unwrap();
-    println!("serialized = {}", serialized);
-
-    let deserialized: NondeterministicFiniteAutomata = serde_yaml::from_str(&serialized).unwrap();
-    println!("deserialized = {:#?}", deserialized);
-
-    match File::create("automata.yaml") {
-        Ok(file) => {
-            let writer = BufWriter::new(file);
-
-            serde_yaml::to_writer(writer, &automata).unwrap();
-        }
-        Err(e) => println!("error : {:?}", e),
-    }
-
-    match File::open("automata.yaml") {
-        Ok(file) => {
-            let reader = BufReader::new(file);
-
-            let automata: NondeterministicFiniteAutomata = serde_yaml::from_reader(reader).unwrap();
-
-            println!("from file = {:#?}", automata);
-        }
-        Err(e) => println!("error : {:?}", e),
-    }
 
     let automata = DeterministicFiniteAutomata::from(&automata);
 
--- formally-cool/src/regular_languages/deterministic_finite_automata.rs
+++ formally-cool/src/regular_languages/deterministic_finite_automata.rs
@@ -1,15 +1,16 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
-
-use serde::{Deserialize, Serialize};
-
 use super::nondeterministic_finite_automata::NondeterministicFiniteAutomata;
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+use std::collections::HashSet;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct DeterministicFiniteAutomata {
+    pub states: BTreeSet<String>,
+    pub alphabet: BTreeSet<String>,
+    pub transition_function: BTreeMap<(String, String), String>,
     pub start_state: String,
-    pub transition_function: HashMap<(String, String), String>,
-    pub accept_states: HashSet<String>,
+    pub accept_states: BTreeSet<String>,
 }
 
 impl DeterministicFiniteAutomata {
@@ -22,12 +23,99 @@ impl DeterministicFiniteAutomata {
     }
 }
 
+pub fn powerset<T: Clone>(slice: &[T]) -> Vec<Vec<T>> {
+    let mut v: Vec<Vec<T>> = Vec::new();
+
+    for mask in 0..(1 << slice.len()) {
+        let mut ss: Vec<T> = vec![];
+        let mut bitset = mask;
+        while bitset > 0 {
+            // isolate the rightmost bit to select one item
+            let rightmost: u64 = bitset & !(bitset - 1);
+            // turn the isolated bit into an array index
+            let idx = rightmost.trailing_zeros();
+            let item = (*slice.get(idx as usize).unwrap()).clone();
+            ss.push(item);
+            // zero the trailing bit
+            bitset &= bitset - 1;
+        }
+        v.push(ss);
+    }
+    v
+}
+
 impl From<&NondeterministicFiniteAutomata> for DeterministicFiniteAutomata {
-    fn from(_automata: &NondeterministicFiniteAutomata) -> Self {
+    fn from(automata: &NondeterministicFiniteAutomata) -> Self {
+        let mut states = BTreeSet::new();
+        let mut alphabet = BTreeSet::new();
+        let mut transition_function = BTreeMap::new();
+        let start_state = automata.start_state.clone() + ".";
+        let mut accept_states = BTreeSet::new();
+
+        for letter in &automata.alphabet {
+            alphabet.insert(letter.clone());
+        }
+
+        {
+            let mut state_set_vec = vec![];
+
+            for state in &automata.states {
+                state_set_vec.push(state.clone());
+            }
+
+            let power_state_set_vec = powerset(&state_set_vec);
+
+            for vec in &power_state_set_vec {
+                let mut state_name = String::new();
+                for state in vec {
+                    state_name = state_name + state + ".";
+                }
+                states.insert(state_name);
+            }
+        }
+
+        for state in &states {
+            for letter in &alphabet {
+                let mut output_state_set = BTreeSet::new();
+
+                let split_state_names: Vec<&str> = state.split_terminator('.').collect();
+
+                for state_name in split_state_names {
+                    if automata.accept_states.contains(state_name) {
+                        accept_states.insert(state.clone());
+                    }
+                    match automata
+                        .transition_function
+                        .get(&(state_name.to_owned(), letter.clone()))
+                    {
+                        Some(set) => {
+                            for s in set {
+                                output_state_set.insert(s.clone());
+                            }
+                        }
+                        None => (),
+                    }
+                }
+
+                println!("{:?}", output_state_set);
+
+                let mut output_state_name = String::new();
+                for state_name in output_state_set {
+                    output_state_name = output_state_name + state_name.as_str() + ".";
+                }
+
+                println!("{:?}", output_state_name);
+
+                transition_function.insert((state.clone(), letter.clone()), output_state_name);
+            }
+        }
+
         DeterministicFiniteAutomata {
-            start_state: String::new(),
-            transition_function: HashMap::new(),
-            accept_states: HashSet::new(),
+            states: states,
+            alphabet: alphabet,
+            transition_function: transition_function,
+            start_state: start_state,
+            accept_states: accept_states,
         }
     }
 }
